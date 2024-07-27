5a5e591995eae587c81c4f166149d87917e7bc81
section01 <section01.act@gmail.com>
2024-07-28 02:12:46 +0900

    Update

--- app/app.py
+++ app/app.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template
 from logging.config import dictConfig
 import yaml
 
+# コンフィグ読込
 with open('./app/config.yml', 'r') as yml:
-    dictConfig(yaml.load(yml, Loader=yaml.Loader))
+    config = yaml.load(yml, Loader=yaml.Loader)
 
+# ロギング設定
+dictConfig(config['logging'])
+
+# FLASKインスタンス生成
 app = Flask(__name__, static_folder='/')
 
+"""
+Function:
+    初期画面を表示する
+Returns:
+    初期画面を返す
+"""
 @app.route('/')
 def main():
     return render_template('index.html')
