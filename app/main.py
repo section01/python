8f309e1e7b195bf12f6704f13a527de3049db59f
section01 <section01.act@gmail.com>
2024-07-28 01:40:22 +0900

    Update

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from logging.config import dictConfig
+import yaml
+
+with open('./app/config.yml', 'r') as yml:
+    dictConfig(yaml.load(yml, Loader=yaml.Loader))
+
+app = Flask(__name__, static_folder='/')
+
+@app.route('/')
+def main():
+    return render_template('index.html')
